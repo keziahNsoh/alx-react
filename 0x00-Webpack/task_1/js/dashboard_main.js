18c58804d5d3b4acaa35ba409ce41095f1c1715f
keziahNsoh <porepeya@gmail.com>
2024-11-04 16:20:48 +0000

    previous assignment

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,18 @@
+import $ from 'jquery';
+import _ from 'lodash';
+
+$(document).ready(function() {
+    $('body').append('<p>Holberton Dashboard</p>');
+    $('body').append('<p>Dashboard data for the students</p>');
+    $('body').append('<button id="clickMe">Click here to get started</button>');
+    $('body').append('<p id="count"></p>');
+    $('body').append('<p>Copyright - Holberton School</p>');
+
+    let count = 0;
+    const updateCounter = () => {
+        count++;
+        $('#count').text(`${count} clicks on the button`);
+    };
+
+    $('#clickMe').on('click', _.debounce(updateCounter, 300));
+});
